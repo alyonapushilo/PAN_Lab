444a1af7df9841225e6e163d284bdc19ccb261b2
Alyona <alyona.pushilo@yyandex.ru>
2016-10-03 18:05:20 +0300

    first progect

---
+++ PAN_1/PAN_1/Calc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAN_1
+{
+    class Calc
+    {
+        public Calc() { }
+        public int Sum (int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Min(int a, int b)
+        {
+            return a - b;
+        }
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+        public int Div(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ PAN_1/PAN_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAN_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calc result = new Calc();
+            Console.WriteLine("Введите число А");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число B");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите операцию");
+
+            string x= Convert.ToString(Console.ReadLine()); ;
+            Console.WriteLine("Операция " + x);
+            switch (x)
+            {
+                case "+":
+                    
+                    Console.WriteLine("Ответ: "+ result.Sum(a, b));
+                    break;
+                case "-":
+                    Console.WriteLine("Ответ: " + result.Min(a, b));
+                    break;
+                case "*":
+                    Console.WriteLine("Ответ: " + result.Mul(a, b));
+                    break;
+                case "/":
+                    Console.WriteLine("Ответ: " + result.Div(a, b));
+                    break;
+                default:
+                    Console.WriteLine("Данной операции нет");
+                    break;
+            }
+
+
+        }
+    }
+}
